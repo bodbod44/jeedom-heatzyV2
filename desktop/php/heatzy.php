2b102556bceeec16053c1068741841e84ec9c96f
bodbod44 <50763752+bodbod44@users.noreply.github.com>
2025-05-08 08:56:25 +0200

    2025-05-08 - Ajout modal debug

--- desktop/php/heatzy.php
+++ desktop/php/heatzy.php
@@ -35,6 +35,12 @@ foreach ($eqLogics as $eqLogic) {
 	    </center>
 	    <span style="font-size : 1.1em;position:relative; top : 15px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;color:#767676"><center>{{Santé}}</center></span>
 	  </div>
+	  <div class="cursor expertModeVisible" id="bt_debugHeatzy" style="background-color : #ffffff; height : 120px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;display:<?php if(log::getLogLevel('heatzy') == 100) echo 'display' ; else echo 'none' ; ?>" >
+	    <center>
+	        <i class="fa fa-search-location" style="font-size : 5em;color:#767676;"></i>
+	    </center>
+	    <span style="font-size : 1.1em;position:relative; top : 15px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;color:#767676"><center>{{Debug}}</center></span>
+	  </div>
 </div>
 <legend><i class="fa fa-cube"></i>  {{Mes Heatzy}}
 </legend>
